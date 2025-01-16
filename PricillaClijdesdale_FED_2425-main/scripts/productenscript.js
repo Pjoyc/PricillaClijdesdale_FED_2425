6f0a44488bd2a0821c52a9cb06e5cdf86ef2f8f9
Pjoyc <clijdep@gmail.com>
2025-01-16 21:01:06 +0100

    Delete PricillaClijdesdale_FED_2425-main directory

--- PricillaClijdesdale_FED_2425-main/scripts/productenscript.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document //
-var openButton = document.querySelector("header > button");
-var deNav = document.querySelector("header nav:nth-of-type(2)");
-
-openButton.onclick = openMenu;
-
-function openMenu() {
-  deNav.classList.toggle("toonMenu");
-}
